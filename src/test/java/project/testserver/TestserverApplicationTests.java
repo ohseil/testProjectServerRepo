2de2b815fd6fefc1beb261b5a606f2da6af87142
ohseil <sioh0612@gmail.com>
2023-02-28 11:57:54 +0900

    feat: 프로젝트 초기 환경 구성

---
+++ src/test/java/project/testserver/TestserverApplicationTests.java
@@ -0,0 +1,13 @@
+package project.testserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
