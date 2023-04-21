1139830484c60ba04a8e89b6ae6df5427b34ef30
bboor <sioh0612@gmail.com>
2023-04-21 20:13:10 +0900

    테스트용 코드 추가7 (#21)
    * 코드 수정 1
    
    * 코드 수정 2

--- src/main/java/project/testserver/auth/presentation/AuthController.java
+++ src/main/java/project/testserver/auth/presentation/AuthController.java
@@ -24,6 +24,8 @@ public class AuthController {
     @GetMapping(value = "/end")
     ResponseEntity<String> end() {
         System.out.println("1");
+        System.out.println("1");
+        System.out.println("1");
         return ResponseEntity.ok("end");
     }
 }
